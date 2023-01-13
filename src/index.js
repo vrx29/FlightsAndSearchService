f917deed6887d80d63766ea98a80e022035d6214
Vineet Kumar <jammackp@gmail.com>
2023-01-13 11:29:57 +0530

    Intiial setup with db creation

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { PORT } = require("./config/serverConfig");
+
+const setupAndStartServer = async () => {
+  // create express object
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+  });
+};
+
+setupAndStartServer();
