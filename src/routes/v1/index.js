94c8c51883771a813bdf09fbc89049b80e357176
Vineet Kumar <jammackp@gmail.com>
2023-01-30 17:22:23 +0530

    FEAT: Added flight create request middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
+const { FlightMiddlewares } = require("../../middlewares/index");
+
 const CityController = require("../../controllers/city-controller");
 const FlightController = require("../../controllers/flight-controller");
+const AirportController = require("../../controllers/airport-controller");
 
 const router = express.Router();
 
@@ -11,7 +14,13 @@ router.get("/city/:id", CityController.get);
 router.get("/city", CityController.getAll);
 router.patch("/city/:id", CityController.update);
 
-router.post("/flights", FlightController.create);
+router.post(
+  "/flights",
+  FlightMiddlewares.validateCreateFlight,
+  FlightController.create
+);
 router.get("/flights", FlightController.getAll);
+
+router.post("/airports", AirportController.create);
 
 module.exports = router;
